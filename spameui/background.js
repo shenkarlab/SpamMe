d3873bde0fd6a7a281abf95484c07dc71fa0249c
DanielShamama <daniel.shama@gmail.com>
2017-01-07 17:43:17 +0200

    add gifs on hover + css

--- spameui/background.js
+++ spameui/background.js
@@ -1,11 +1,3 @@
-// // Called when the user clicks on the browser action.
-// chrome.browserAction.onClicked.addListener(function(tab) {
-//   // No tabs or host permissions needed!
-//   console.log('Turning ' + tab.url + ' red!');
-
-//   gmailAPILoaded();
-// });
-
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
   if (changeInfo.status == 'complete' && tab.active) {
       if(tab.url.includes("#spam")){
@@ -88,7 +80,8 @@ function sendMessagestoCategories(msgs){
       massages: generateRequestData(msgs)
     },
     success: function( data ) {
-      drawResult(data)
+        console.log("success" + data);
+        drawResult(data.categoriesRate);
     }
   });
 }
@@ -105,7 +98,67 @@ function generateRequestData(data){
     return result;
 }
 
+function createTreemapFromRespone(data) {
+    var result = [];
+    var colors = ["#629AF1", "#1BB15A", "#F9C21A", "#D74A39", "#8038CC"];
+    var stringRes = "[";
+    data.forEach(function(el, index){
+        if(el.subCategories.length != 0) {
+            result.push({
+                label: el.name,
+                value: null,
+                color: colors[index]
+            });
+
+            el.subCategories.forEach(function(sub){
+                result.push({
+                    label: sub.name,
+                    value: sub.emailsCount,
+                    parent: el.name,
+                    data: { description: sub.emailsCount + " Emails", title: el.name + "#" + sub.name }
+                });
+            });   
+        }
+    });
+    
+    stringRes = stringRes.substr(0, stringRes.length-1);
+    stringRes += "]";
+    
+    console.log("after createTreeMap : \n");
+    console.log(result);
+    
+    return `
+         $(function () {
+                $('#treemap').jqxTreeMap({
+                        width: 1047,
+                        height: 250,
+                        source: ` + returnMockJson() + `,
+                        colorRange: 1,
+                        renderCallbacks: {
+                            '*': function (sectorHtmlElement, sectorData) {
+                               
+                                sectorHtmlElement.hover(function(){
+                                    if($(this).attr('class').indexOf('jqx-treemap-rectangle-parent') != -1) return;
+    
+                                    $(this).css('background-image','url('+chrome.extension.getURL('treemap/images/'+$(this).find('span').text() + '.gif')+')');
+                                    $(this).css('background-size','cover');
+                                }, function(){
+ $(this).css('background-image','');
+});
+                            }
+                        }
+                    });
+                $('.jqx-treemap-label').css('position','');
+                $('.jqx-treemap-rectangle .jqx-treemap-label').css('position','relative').css('left','0px');
+                $('.jqx-treemap-rectangle-hover').each(function(index,obj){
+                    $(this).css('background-color', '#F00');
+                });
+        });`;
+
+}
+
 function drawResult(results){
+    console.log(results);
     chrome.tabs.executeScript(null, { file: "treemap/jqx.base.css" } , function() {
             chrome.tabs.executeScript(null, { file: "jquery-3.1.1.min.js"}, function(){
                   chrome.tabs.executeScript(null, { file: "treemap/jqxcore.js"}, function(){
@@ -121,7 +174,7 @@ function drawResult(results){
                                                     document.getElementById(':4').insertBefore(newEl, document.getElementById(':2'));
 
                                                     var script = document.createElement("script");
-                                                    script.text = `+templateHTml+ `;
+                                                    script.text = `+ createTreemapFromRespone(results) + `;
 
                                                     document.body.appendChild(script);
                                                 `
@@ -149,10 +202,9 @@ function getMessageByID(_id){
 	}); 
 }
 
-var templateHTml = `
- $(function () {
-            var data = [
-            {
+function returnMockJson(){
+    
+           return `[ {
                 label: 'Amotions',
                 value: null,
                 color: '#629AF1'
@@ -178,49 +230,49 @@ var templateHTml = `
                 color: '#8038CC'
             },
             {
-                label: 'Happy',
+                label: 'happy',
                 value: 5,
                 parent: 'Amotions',
                 data: { description: "4 Emails", title: "Life-Style#Family" }
             },
             {
-                label: 'Greeting',
+                label: 'greeting',
                 value: 5,
                 parent: 'Amotions',
                 data: { description: "5 Emails", title: "Commerce#Offers" }
             },
             {
-                label: 'Pressure',
+                label: 'lost',
                 value: 1,
                 parent: 'Amotions',
                 data: { description: "4 Emails5 Emails", title: "Commerce#Sells" }
             },
             {
-                label: 'Success',
+                label: 'success',
                 value: 2,
                 parent: 'Amotions',
                 data: { description: "1 Emails", title: "Commerce#Hi-tech" }
             },
             {
-                label: 'Family',
+                label: 'family',
                 value: 4,
                 parent: 'Life-Style',
                 data: { description: "4 Emails", title: "Life-Style#Family" }
             },
             {
-                label: 'Offers',
+                label: 'offers',
                 value: 5,
                 parent: 'Commerce',
                 data: { description: "5 Emails", title: "Commerce#Offers" }
             },
             {
-                label: 'Sells',
+                label: 'sells',
                 value: 4,
                 parent: 'Commerce',
                 data: { description: "4 Emails5 Emails", title: "Commerce#Sells" }
             },
             {
-                label: 'Hi-tech',
+                label: 'hi-tech',
                 value: 1,
                 parent: 'Commerce',
                 data: { description: "1 Emails", title: "Commerce#Hi-tech" }
@@ -238,27 +290,134 @@ var templateHTml = `
                 data: { description: "5 Emails", title: "Advertising#Marketing" }
             },
             {
-                label: 'Discounts',
+                label: 'discounts',
                 value: 1,
                 parent: 'Advertising',
                 data: { description: "1 Emails", title: "Advertising#Discounts" }
             },
             {
-                label: 'Personal',
+                label: 'personal',
                 value: 4,
                 parent: 'Financial',
                 data: { description: "4 Emails", title: "Financial#Personal" }
             }
             
-            ];
-  
-            
+            ]`;
+}
 
-            $('#treemap').jqxTreeMap({
-                width: 1047,
-                height: 250,
-                source: data,
-                colorRange: 1
-            });
-        });
-`;
+
+//var templateHTml = `
+// $(function () {
+//            var data = [
+//            {
+//                label: 'Amotions',
+//                value: null,
+//                color: '#629AF1'
+//            },
+//            {
+//                label: 'Life-Style',
+//                value: null,
+//                color: '#1BB15A'
+//            },
+//            {
+//                label: 'Commerce',
+//                value: null,
+//                color: '#F9C21A'
+//            },
+//            {
+//                label: 'Financial',
+//                value: null,
+//                color: '#D74A39'
+//            },
+//            {
+//                label: 'Advertising',
+//                value: null,
+//                color: '#8038CC'
+//            },
+//            {
+//                label: 'Happy',
+//                value: 5,
+//                parent: 'Amotions',
+//                data: { description: "4 Emails", title: "Life-Style#Family" }
+//            },
+//            {
+//                label: 'Greeting',
+//                value: 5,
+//                parent: 'Amotions',
+//                data: { description: "5 Emails", title: "Commerce#Offers" }
+//            },
+//            {
+//                label: 'Pressure',
+//                value: 1,
+//                parent: 'Amotions',
+//                data: { description: "4 Emails5 Emails", title: "Commerce#Sells" }
+//            },
+//            {
+//                label: 'Success',
+//                value: 2,
+//                parent: 'Amotions',
+//                data: { description: "1 Emails", title: "Commerce#Hi-tech" }
+//            },
+//            {
+//                label: 'Family',
+//                value: 4,
+//                parent: 'Life-Style',
+//                data: { description: "4 Emails", title: "Life-Style#Family" }
+//            },
+//            {
+//                label: 'Offers',
+//                value: 5,
+//                parent: 'Commerce',
+//                data: { description: "5 Emails", title: "Commerce#Offers" }
+//            },
+//            {
+//                label: 'Sells',
+//                value: 4,
+//                parent: 'Commerce',
+//                data: { description: "4 Emails5 Emails", title: "Commerce#Sells" }
+//            },
+//            {
+//                label: 'Hi-tech',
+//                value: 1,
+//                parent: 'Commerce',
+//                data: { description: "1 Emails", title: "Commerce#Hi-tech" }
+//            },
+//            {
+//                label: 'buy',
+//                value: 1,
+//                parent: 'Commerce',
+//                data: { description: "1 Emails", title: "Commerce#Buy" }
+//            },
+//            {
+//                label: 'marketing',
+//                value: 5,
+//                parent: 'Advertising',
+//                data: { description: "5 Emails", title: "Advertising#Marketing" }
+//            },
+//            {
+//                label: 'Discounts',
+//                value: 1,
+//                parent: 'Advertising',
+//                data: { description: "1 Emails", title: "Advertising#Discounts" }
+//            },
+//            {
+//                label: 'Personal',
+//                value: 4,
+//                parent: 'Financial',
+//                data: { description: "4 Emails", title: "Financial#Personal" }
+//            }
+//            
+//            ];
+//  
+//            
+//
+//            $('#treemap').jqxTreeMap({
+//                width: 1047,
+//                height: 250,
+//                source: data,
+//                colorRange: 1
+//            });
+//            $('.jqx-treemap-label').css('position','');
+//            $('.jqx-treemap-rectangle .jqx-treemap-label').css('position','relative').css('left','0px');
+//        });
+//`;
