06879ef44b6959f404b1e7ac7edfcdf45feb99d6
DanielShamama <daniel.shama@gmail.com>
2017-01-26 18:50:50 +0200

    responsive layout

--- spameui/background.js
+++ spameui/background.js
@@ -223,6 +223,7 @@ function createTreemapFromRespone(data) {
     
     //convert the response data for treemap
     data.forEach(function(el, index){
+        console.log(el);
         if(el.subCategories.length != 0) {
             result.push({
                 label: el.name,
@@ -248,8 +249,9 @@ function createTreemapFromRespone(data) {
          $(function () {
             $(document).ready(function() {
                 $('#treemap').jqxTreeMap({
-                        width: 1047,
-                        height: 250,
+                        width: '100%',
+                        selectionEnabled: false,
+                        height: 400,
                         source: ` + JSON.stringify(result) + `,
                         colorRange: 1,
                         renderCallbacks: {
